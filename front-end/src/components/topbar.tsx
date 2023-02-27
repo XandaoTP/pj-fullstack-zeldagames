85d9b3530924b25e934e36e65106b891b82e48f3
unknown <alexandre8603@gmail.com>
2023-02-27 13:07:54 -0300

    add input search

--- front-end/src/components/topbar.tsx
+++ front-end/src/components/topbar.tsx
@@ -1,9 +1,45 @@
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo.png';
+import { useRef, useState } from "react";
+import '../assets/css/topbar.css'
+
 
 export function TopBar () {
+    const [isOpen, setIsOpen] = useState("");
+    const [search, setSearch] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleClick = () => {
+        if(!isOpen) {
+            inputRef.current?.focus();
+        }
+        setIsOpen(!Boolean(isOpen) ? "open" : "")
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearch(e.target.value);
+        console.log(search)
     return (
-        <header className="flex justify-center bg-stone-900">
+        <header className="flex justify-between ">
             <img src={logo} alt="The legend of Zelda" className="py-5 w-20"/> 
+            <div className="relative text-center">
+                <div className={` search ${isOpen}`}>
+                    <input
+                        ref={inputRef}
+                        onChange={handleChange}
+                        placeholder="Find a zelda Game"
+                        type="text" 
+                    />
+                    <button
+                        onClick={handleClick}
+                        className={
+                            `uil uil-${
+                                isOpen ?
+                                "multiply" : "search"}`
+                            }>
+                    </button>
+                    
+                </div>         
+            </div>
         </header>
     )
 }
