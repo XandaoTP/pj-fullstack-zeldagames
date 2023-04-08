9632fbc7c987f3edb4058a966628be836106fd44
unknown <alexandre8603@gmail.com>
2023-04-08 09:24:07 -0300

    create mysql connection and finish crete table

--- back-end/src/db/scripts/createTables.ts
+++ back-end/src/db/scripts/createTables.ts
@@ -3,7 +3,19 @@ import { requireSQL } from '../requireSQL';
 
 async function creteTables () {
     const createTableMysql = await requireSQL('createTable.sql')
-    console.log(createTableMysql)
+
+    const connection = await mysql.createConnection({
+      host: '127.0.0.1',
+      port: 6000,
+      user: 'root',
+      password: 'password',
+      database: 'zeldagames',
+      multipleStatements: true 
+    })
+    
+    const response = await connection.query(createTableMysql)
+    console.log(connection)
+    connection.destroy();
 }
 
 creteTables();
--- back-end/src/db/sql/createTable.sql
+++ back-end/src/db/sql/createTable.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS games;
 CREATE TABLE games (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    picture VARCHAR(1000),
+    picture VARCHAR(255),
     content TEXT NOT NULL,
-    subtitle VARCHAR(255)
-    createdAt DATATIME DEFAULT NOW()
-);
+    subtitle VARCHAR(255),
+    created_at DATETIME DEFAULT NOW()
+    );
