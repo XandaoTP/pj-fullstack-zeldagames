21b6a5c0c092a094d0ecff7f417a13d595d2adfa
unknown <alexandre8603@gmail.com>
2023-04-08 09:48:00 -0300

    add dotenv

--- back-end/src/db/scripts/createTables.ts
+++ back-end/src/db/scripts/createTables.ts
@@ -1,15 +1,18 @@
 import mysql from 'mysql2/promise';
 import { requireSQL } from '../requireSQL';
+require('dotenv').config()
+
+console.log(process.env.DB_HOST);
 
 async function creteTables () {
     const createTableMysql = await requireSQL('createTable.sql')
 
     const connection = await mysql.createConnection({
-      host: '127.0.0.1',
-      port: 6000,
-      user: 'root',
-      password: 'password',
-      database: 'zeldagames',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       multipleStatements: true 
     })
     
